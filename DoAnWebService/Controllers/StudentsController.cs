1b05c29932209d9a42f8c785b219c64805666fa5
ADMIN\ADMIN <leelamhair@gmail.com>
2026-04-14 20:29:57 +0700

    wraper api

--- DoAnWebService/Controllers/StudentsController.cs
+++ DoAnWebService/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using DoAnWebService.Data;
 using DoAnWebService.Models;
+using DoAnWebService.Utlis;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,9 +18,42 @@ namespace DoAnWebService.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Sinhvien>>> GetStudents()
+        public async Task<ActionResult<ApiResponse<List<Sinhvien>>>> GetStudents()
         {
-            return await _context.Sinhviens.ToListAsync();
+            var students = await _context.Sinhviens.ToListAsync();
+
+            return Ok(new ApiResponse<List<Sinhvien>>
+            {
+                StatusCode = 200,
+                Success = true,
+                Message = "Lấy danh sách sinh viên thành công.",
+                Data = students
+            });
+        }
+
+        [HttpGet("{masv}")]
+        public async Task<ActionResult<Sinhvien>> GetStudentByMaSV(string masv)
+        {
+            var student = await _context.Sinhviens.FirstOrDefaultAsync(sv => sv.Masv == masv);
+
+            if (student == null)
+            {
+                return NotFound(new ApiResponse<Sinhvien>
+                {
+                    StatusCode = 404,
+                    Success = false,
+                    Message = "Không tìm thấy sinh viên.",
+                    Data = null
+                });
+            }
+
+            return Ok(new ApiResponse<Sinhvien>
+            {
+                StatusCode = 200,
+                Success = true,
+                Message = "Tìm sinh viên thành công",
+                Data = student
+            });
         }
     }
 }
---
+++ DoAnWebService/Utils/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace DoAnWebService.Utlis
+{
+    public class ApiResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+    }
+}
