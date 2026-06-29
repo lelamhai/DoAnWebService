c8fb561ac7287b8ba6fecae8a17585317bdfa624
ADMIN\ADMIN <leelamhair@gmail.com>
2026-06-29 23:35:37 +0700

    load point student

--- DoAnWebService/Controllers/StudentController.cs
+++ DoAnWebService/Controllers/StudentController.cs
@@ -168,7 +168,6 @@ namespace DoAnWebService.Controllers
             });
         }
 
-
         [HttpDelete("delete/{masv}")]
         public async Task<IActionResult> Delete(string masv)
         {
@@ -415,6 +414,72 @@ namespace DoAnWebService.Controllers
                     message = "Lỗi hệ thống"
                 });
             }
+        }
+
+        [HttpGet("get-point-student")]
+        public async Task<IActionResult> GetPoint(string masv, int page)
+        {
+            List<PointStudent> list = new();
+            using (SqlConnection conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                using (SqlCommand cmd = new SqlCommand("SP_LAYDIEM_SV", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@MASV", masv);
+                    await conn.OpenAsync();
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            list.Add(new PointStudent
+                            {
+                                MaMH = reader["MAMH"] == DBNull.Value
+                                ? null
+                                : reader["MAMH"].ToString(),
+
+                                                TenMH = reader["TENMH"] == DBNull.Value
+                                ? null
+                                : reader["TENMH"].ToString(),
+
+                                                SoTinChi = reader["SOTINCHI"] == DBNull.Value
+                                ? 0
+                                : Convert.ToInt32(reader["SOTINCHI"]),
+
+                                                NienKhoa = reader["NIENKHOA"] == DBNull.Value
+                                ? null
+                                : reader["NIENKHOA"].ToString(),
+
+                                                HocKy = reader["HOCKY"] == DBNull.Value
+                                ? 0
+                                : Convert.ToInt32(reader["HOCKY"]),
+
+                                                DiemCC = reader["DIEM_CC"] == DBNull.Value
+                                ? null
+                                : Convert.ToDecimal(reader["DIEM_CC"]),
+
+                                                DiemGK = reader["DIEM_GK"] == DBNull.Value
+                                ? null
+                                : Convert.ToDecimal(reader["DIEM_GK"]),
+
+                                                DiemCK = reader["DIEM_CK"] == DBNull.Value
+                                ? null
+                                : Convert.ToDecimal(reader["DIEM_CK"]),
+
+                                                DiemTong = reader["DIEM_TONG"] == DBNull.Value
+                                ? null
+                                : Convert.ToDecimal(reader["DIEM_TONG"])
+                            });
+                        }
+                    }
+                }
+            }
+
+            var result = PaginationHelper.CreatePagedResult(list, page, -1);
+            return Ok(new APIResponse<PagedResult<PointStudent>>
+            {
+                Message = "Lấy danh sách điểm sinh viên thành công.",
+                Data = result
+            });
         }
 
     }
---
+++ DoAnWebService/DTO/Student/PointStudent.cs
@@ -0,0 +1,23 @@
+﻿namespace DoAnWebService.DTO.Student
+{
+    public class PointStudent
+    {
+        public string? MaMH { get; set; }
+
+        public string? TenMH { get; set; }
+
+        public int SoTinChi { get; set; }
+
+        public string? NienKhoa { get; set; }
+
+        public int HocKy { get; set; }
+
+        public decimal? DiemCC { get; set; }
+
+        public decimal? DiemGK { get; set; }
+
+        public decimal? DiemCK { get; set; }
+
+        public decimal? DiemTong { get; set; }
+    }
+}
